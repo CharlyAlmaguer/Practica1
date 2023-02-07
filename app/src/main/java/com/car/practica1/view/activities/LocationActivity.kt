59fc4c292d21de7cff411811781dc3fd1025ddfd
carlosalmaguer <calmaguer.almaguer@gmail.com>
2023-02-06 19:33:15 -0600

    Commit Practica 2.1

--- app/src/main/java/com/car/practica1/view/activities/LocationActivity.kt
+++ app/src/main/java/com/car/practica1/view/activities/LocationActivity.kt
@@ -23,7 +23,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class LocationActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+class LocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityLocationBinding
 
@@ -48,101 +48,9 @@ class LocationActivity : AppCompatActivity(), OnMapReadyCallback, LocationListen
 
     }
 
-    private fun updateOrRequestPermissions() {
-
-        //Revisando los permisos
-        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-
-        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-
-        coarseLocationPermissionGranted = hasCoarseLocationPermission
-        fineLocationPermissionGranted = hasFineLocationPermission
-
-        //Solicitando los permisos
-
-        val permissionsToRequest = mutableListOf<String>()
-
-        if (!hasCoarseLocationPermission)
-            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-
-        if (!hasFineLocationPermission)
-            permissionsToRequest.add(Manifest.permission.ACCESS_FINE_LOCATION)
-
-
-        if (permissionsToRequest.isNotEmpty()) {
-            ActivityCompat.requestPermissions(
-                this,
-                permissionsToRequest.toTypedArray(),
-                PERMISO_LOCALIZACION
-            )
-        } else {
-            //Tenemos los permisos
-            map.isMyLocationEnabled = true
-            //Ubicacion del usuario
-            val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10F, this)
-
-        }
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            PERMISO_LOCALIZACION -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //Se obtuvo el permiso
-                    updateOrRequestPermissions()
-                } else {
-                    if (shouldShowRequestPermissionRationale(permissions[0])) {
-                        AlertDialog.Builder(this)
-                            .setTitle("Permiso requerido")
-                            .setMessage("Se necesita acceder a la ubicación para esta funcionalidad")
-                            .setPositiveButton(
-                                "Entendido",
-                                DialogInterface.OnClickListener { _, _ ->
-                                    updateOrRequestPermissions()
-                                })
-                            .setNegativeButton(
-                                "Salir",
-                                DialogInterface.OnClickListener { dialog, _ ->
-                                    dialog.dismiss()
-                                    finish()
-                                })
-                            .create()
-                            .show()
-                    } else {
-                        //Si el usuario no quiere que nunca se le vuelva a preguntar por el permiso
-                        Toast.makeText(this, "El permiso a la localización se ha negado permanentemente", Toast.LENGTH_LONG).show()
-                        finish()
-                    }
-                }
-            }
-        }
-    }
-
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         createMarker()
-        updateOrRequestPermissions()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        if(!::map.isInitialized) return
-        if (!fineLocationPermissionGranted){
-            updateOrRequestPermissions()
-        }
     }
 
     fun createMarker(){
@@ -161,10 +69,6 @@ class LocationActivity : AppCompatActivity(), OnMapReadyCallback, LocationListen
 
 
         map.addMarker(marker)
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15f), 4000, null)
-    }
-
-    override fun onLocationChanged(location: Location) {
-
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15f), 5000, null)
     }
 }
